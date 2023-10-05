de2a98b309983236b01015f51d71d1d22d816379
Imrglop <69129770+Imrglop@users.noreply.github.com>
2023-10-05 16:25:05 -0400

    update Debug script, added NBSRadio

--- Scripts/Debug/index.js
+++ Scripts/Debug/index.js
@@ -1,3 +1,5 @@
+/// <reference path="./definitions/index.d.ts" />
+
 "notrust"; // tells latite api to not trust this script so it won't be abused
 let cmd = new Command("execute", "Execute JavaScript code.", "$ [...]", ["exec"]);
 
@@ -17,15 +19,22 @@ let colors = {
     "boolean": TextColor.MINECOIN_GOLD
 }
 
-cmd.on("execute", (_, /** @type {string[]} */ args) => {
-    if (args.length == 0) return false;
+function stringify(val) {
+	if (typeof val === "string") {
+		return JSON.stringify(val);
+	}
+	
+	return val + '';
+}
 
-    // this is a hacky way becuase commandLine is not implemented
-    let code = args.join(" ");
+cmd.on("execute", (label, args, commandLine) => {
+    if (args.length === 0) return false;
+
+    let code = commandLine.trim().substring(client.getCommandManager().getPrefix().length + label.length).trimStart();
     script.log(TextColor.GRAY + "> " + code)
     try {
         let ret = eval(code);
-        script.log(`${colors[typeof ret]}${JSON.stringify(ret)}`)
+        script.log(`${colors[typeof ret]}${stringify(ret)}`)
     } catch (e) {
         script.log(`${TextColor.RED}` + e);
     }
