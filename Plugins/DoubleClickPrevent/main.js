912890f188a3c5f45eee185c6655e6f6cfcc6434
Imrglop <69129770+Imrglop@users.noreply.github.com>
2024-01-24 12:41:48 -0500

    add plugin

---
+++ Plugins/DoubleClickPrevent/main.js
@@ -0,0 +1,25 @@
+let mod = new Module("doubleClickPrevent", "Double Click Prevent", "Adds a window after clicking where subsequent ones are ignored. (Only works with mouse buttons)", 0);
+client.getModuleManager().registerModule(mod);
+client.on("unload-script", scr => {
+    if (scr.scriptName === "DoubleClickPrevent") {
+        client.getModuleManager().deregisterModule(mod);
+    }
+});
+let optDebounce = mod.addNumberSetting("debounce", "Debounce Time (ms)", "Window where subsequent clicks are ignored", 0, 50, 1, 0);
+let lastValidClickTime = 0;
+client.on("click", e => {
+    if (!mod.isEnabled())
+        return;
+    if (!e.isDown)
+        return;
+    if (!(e.button == 1 || e.button == 2))
+        return;
+    let clickTime = Date.now();
+    if (lastValidClickTime + optDebounce.getValue() > clickTime) {
+        e.cancel = true;
+    }
+    else {
+        e.cancel = false;
+        lastValidClickTime = clickTime;
+    }
+});
