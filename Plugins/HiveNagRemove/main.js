3f79ff2069308f343308b06ce4133465f423268d
Gabriëlla <meow@hehe.moe>
2024-10-26 01:18:28 +0200

    Add HiveNagRemove plugin

---
+++ Plugins/HiveNagRemove/main.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let module = new Module("HiveNagRemove", "Hive Nag Remove", "Removes the naggy messages on the hive.", 0);
+client.getModuleManager().registerModule(module);
+
+client.on('receive-chat', evt => {
+    if(evt.message.startsWith('\xa76[\xa7e!\xa76]')) evt.cancel = true
+})
+
+client.on("unload-script", evt => {
+    if (evt.scriptName != script.name) return
+    client.getModuleManager().deregisterModule(module);
+});
