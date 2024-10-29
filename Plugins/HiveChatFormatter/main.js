87b4e16604a6207dddba12beffd5f344a3892025
DimitrovN <drnoversey@gmail.com>
2024-10-29 19:03:53 +0100

    Fix: Swap defaults

--- Plugins/HiveChatFormatter/main.js
+++ Plugins/HiveChatFormatter/main.js
@@ -3,8 +3,8 @@
 let moduleDescription = `Format the Hive Chat to your liking with these colors:\n\t 0 - Black\n\t 1 - Dark Blue\n\t 2 - Dark Green\n\t 3 - Dark Aqua\n\t 4 - Dark Red\n\t 5 - Dark Purple\n\t 6 - Gold\n\t 7 - Gray (Regular Default)\n\t 8 - Dark Gray\n\t 9 - Blue\n\t10 - Green (Hive+ Default)\n\t11 - Aqua\n\t12 - Red\n\t13 - Light Purple\n\t14 - Yellow\n\t15 - White`;
 let module = new Module("HiveChatFormatter", "Hive Chat Formatter", moduleDescription, 0);
 
-let plus = module.addNumberSetting('plus', 'Hive+', 'Hive+', 0, 15, 1, 7);
-let reg  = module.addNumberSetting('reg', 'Regular', 'Regular', 0, 15, 1, 10);
+let plus = module.addNumberSetting('plus', 'Hive+', 'Hive+', 0, 15, 1, 10);
+let reg  = module.addNumberSetting('reg', 'Regular', 'Regular', 0, 15, 1, 7);
 
 client.getModuleManager().registerModule(module);
 
