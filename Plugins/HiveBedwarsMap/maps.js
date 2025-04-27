7f9bfe4dfa50d4e48cc34e27673fe01625c310e3
Goobie <meow@hehe.moe>
2025-04-28 00:16:33 +0200

    Updated HiveBedwarsMap to include newer maps and be more reliable by using set-score

--- Plugins/HiveBedwarsMap/maps.js
+++ Plugins/HiveBedwarsMap/maps.js
@@ -1,17 +1,29 @@
 Object.defineProperty(exports, '__esModule', { value: true })
 
 const x = i => Texture.load(i)
+const y = i => String.fromCharCode(i)
 
 exports.textures = {
-    "red":     { normal: x("./textures/red.png"),     broken: x("./textures/red_broken.png"),     eliminated: x("./textures/red_eliminated.png") },
-    "yellow":  { normal: x("./textures/yellow.png"),  broken: x("./textures/yellow_broken.png"),  eliminated: x("./textures/yellow_eliminated.png") },
-    "gray":    { normal: x("./textures/gray.png"),    broken: x("./textures/gray_broken.png"),    eliminated: x("./textures/gray_eliminated.png") },
-    "blue":    { normal: x("./textures/blue.png"),    broken: x("./textures/blue_broken.png"),    eliminated: x("./textures/blue_eliminated.png") },
-    "orange":  { normal: x("./textures/orange.png"),  broken: x("./textures/orange_broken.png"),  eliminated: x("./textures/orange_eliminated.png") },
-    "aqua":    { normal: x("./textures/aqua.png"),    broken: x("./textures/aqua_broken.png"),    eliminated: x("./textures/aqua_eliminated.png") },
-    "green":   { normal: x("./textures/green.png"),   broken: x("./textures/green_broken.png"),   eliminated: x("./textures/green_eliminated.png") },
-    "magenta": { normal: x("./textures/magenta.png"), broken: x("./textures/magenta_broken.png"), eliminated: x("./textures/magenta_eliminated.png") },
+    "red":     { normal: x("./red.png"),     broken: x("./red_broken.png"),     eliminated: x("./red_eliminated.png") },
+    "yellow":  { normal: x("./yellow.png"),  broken: x("./yellow_broken.png"),  eliminated: x("./yellow_eliminated.png") },
+    "gray":    { normal: x("./gray.png"),    broken: x("./gray_broken.png"),    eliminated: x("./gray_eliminated.png") },
+    "blue":    { normal: x("./blue.png"),    broken: x("./blue_broken.png"),    eliminated: x("./blue_eliminated.png") },
+    "orange":  { normal: x("./orange.png"),  broken: x("./orange_broken.png"),  eliminated: x("./orange_eliminated.png") },
+    "aqua":    { normal: x("./aqua.png"),    broken: x("./aqua_broken.png"),    eliminated: x("./aqua_eliminated.png") },
+    "green":   { normal: x("./green.png"),   broken: x("./green_broken.png"),   eliminated: x("./green_eliminated.png") },
+    "magenta": { normal: x("./magenta.png"), broken: x("./magenta_broken.png"), eliminated: x("./magenta_eliminated.png") },
 }
+
+exports.teamIcons = [
+    [ "red",     y(0xE1DC), y(0xE1DD) ],
+    [ "yellow",  y(0xE1DE), y(0xE1DF) ],
+    [ "gray",    y(0xE1D4), y(0xE1D5) ],
+    [ "blue",    y(0xE1D2), y(0xE1D3) ],
+    [ "orange",  y(0xE1DA), y(0xE1DB) ],
+    [ "aqua",    y(0xE1D0), y(0xE1D1) ],
+    [ "green",   y(0xE1D6), y(0xE1D7) ],
+    [ "magenta", y(0xE1D8), y(0xE1D9) ]
+]
 
 exports.maps = {
     "default": {
@@ -43,6 +55,19 @@ exports.maps = {
         ]
     },
     "Oceanic": {
+        "distance": 50,
+        "islands": [
+            { "angle": 247.5, "team": "red"     },
+            { "angle": 292.5, "team": "yellow"  },
+            { "angle": 337.5, "team": "gray"    },
+            { "angle": 22.50, "team": "blue"    },
+            { "angle": 67.50, "team": "orange"  },
+            { "angle": 112.5, "team": "aqua"    },
+            { "angle": 157.5, "team": "green"   },
+            { "angle": 202.5, "team": "magenta" }
+        ]
+    },
+    "Enchantment": {
         "distance": 50,
         "islands": [
             { "angle": 247.5, "team": "red"     },
@@ -148,6 +173,15 @@ exports.maps = {
             { "angle": 270.0, "team": "green"  }
         ]
     },
+    "Lumora": {
+        "distance": 30,
+        "islands": [
+            { "angle": 90.00, "team": "red"    },
+            { "angle": 180.0, "team": "yellow" },
+            { "angle": 270.0, "team": "blue"   },
+            { "angle": 0.000, "team": "green"  }
+        ]
+    },
     "Monster Labs": {
         "distance": 30,
         "islands": [
@@ -220,6 +254,15 @@ exports.maps = {
             { "angle": 90.00, "team": "green"  }
         ]
     },
+    "Soul's End": {
+        "distance": 30,
+        "islands": [
+            { "angle": 180.0, "team": "red"    },
+            { "angle": 270.0, "team": "yellow" },
+            { "angle": 0.000, "team": "blue"   },
+            { "angle": 90.00, "team": "green"  }
+        ]
+    },
     "Spring": {
         "distance": 30,
         "islands": [
@@ -275,6 +318,17 @@ exports.maps = {
         ]
     },
     "Winter": {
+        "distance": 30,
+        "islands": [
+            { "angle": 180.0, "team": "red"    },
+            { "angle": 270.0, "team": "yellow" },
+            { "angle": 0.000, "team": "blue"   },
+            { "angle": 90.00, "team": "green"  }
+        ]
+    },
+
+    // LTM
+    "Manor": {
         "distance": 30,
         "islands": [
             { "angle": 180.0, "team": "red"    },
